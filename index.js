4373d3ed0535cc881e79d7bfd71ad1de19c88005
zacherysims <zach.md.sims97@gmail.com>
2020-11-13 17:42:45 -0700

    All done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { randomInt } = require('crypto');
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var cookie = require('cookie');
+
+let usernames = []
+let messages = []
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -10,10 +15,87 @@ app.get('/', (req, res) => {
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+    let addedUser = false;
+    cookie = socket.request.headers.cookie;
+    if (cookie) {
+        oldName = cookie.split("=")[1];
+        if (oldName) {
+            socket.username = oldName;
+        }
+    }
+    else {
+        socket.username = randomInt(1000).toString();
+    }
+
+    socket.on('add user', () => {
+        if (addedUser) return;
+
+        socket.colour = '000000';
+        usernames.push(socket.username);
+        addedUser = true;
+        socket.emit('welcome', socket.username, messages);
+        io.emit('new user', usernames);
     });
-  });
+    socket.on('new message', (msg) => {
+        message = {
+            username: socket.username,
+            message: msg,
+            colour: socket.colour,
+            timestamp: timeStamp()
+        }
+        console.log(socket.colour);
+        messages.push(message);
+        io.emit('new message', messages);
+    });
+    socket.on('changed name', (newName) => {
+        let userIndex = usernames.indexOf(socket.username);
+        messages.forEach(element => {
+            if (element.username == socket.username) {
+                element.username = newName;
+            }
+        });
+        socket.username = newName;
+        console.log(userIndex);
+        usernames[userIndex] = socket.username;
+        io.emit('changed name', usernames);
+        io.emit('new message', messages);
+        console.log(usernames);
+    });
+    socket.on('changed colour', (newColour) => {
+        messages.forEach(element => {
+            if (element.username == socket.username) {
+                element.colour = newColour;
+            }
+        });
+        io.emit('new message', messages);
+        socket.colour = newColour;
+    })
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            let userIndex = usernames.indexOf(socket.username);
+            usernames.splice(userIndex, 1);
+            io.emit('user left', usernames);
+        }
+    })
+});
+
 http.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
+function timeStamp() {
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    let mins = date.getMinutes();
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+    let secs = date.getSeconds();
+    if (secs < 10) {
+        secs = '0' + secs;
+    }
+    return hours + ':' + mins + ':' + secs + ' ';
+}
--- public/main.js
+++ public/main.js
@@ -1,17 +1,109 @@
 
-  $(function () {
+$(function () {
 
     //Variables being initialized from the DOM, jquery-style
     let $messageArea = $('.messageArea');
     let $messageText = $('.messageText');
-    var socket = io();
-    $('form').submit(function(e){
-      e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
+    let $userList = $('.userList');
+    let $window = $(window);
+
+
+    let username = false;
+    let connected = false;
+    let colour = '000000';
+
+    let socket = io();
+
+    socket.emit('add user');
+
+    socket.on('welcome', (assignedUsername, messages) => {
+        connected = true;
+        username = assignedUsername;
+        document.cookie = "username=" + assignedUsername;
+        messages.forEach(element => addMessage(element));
     });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
+
+    socket.on('new message', (messages) => {
+        $messageArea.empty();
+        messages.forEach(element => addMessage(element));
+    })
+
+    socket.on('new user', (usernames) => {
+        $userList.empty();
+        usernames.forEach(element => addUser(element));
+    })
+
+    socket.on('user left', (usernames) => {
+        $userList.empty();
+        usernames.forEach(element => addUser(element));
+    })
+
+    socket.on('changed name', (usernames) => {
+        $userList.empty();
+        usernames.forEach(element => addUser(element));
+    })
+
+    const addUser = (addedUsername) => {
+        let $usernameDiv = $('<span class="username">').text(addedUsername);
+        if(username == addedUsername)
+        {
+            $usernameDiv = $('<span class="username">').text(addedUsername + ' (you)');
+        }
+        let $userDiv = $('<span class="user">').append($usernameDiv);
+        $userList.append($userDiv);
+    }
+
+    const changeColour = (newColour) => {
+        colour = newColour;
+        socket.emit('changed colour', newColour);
+    }
+
+    const changeName = (newName) => {
+        username = newName;
+        document.cookie = "username=" + newName;
+        socket.emit('changed name', newName);
+    }
+
+    const sendMessage = () => {
+        let message = $messageText.val();
+
+        if (message && connected) {
+            $messageText.val('');
+            if (message.split(" ")[0] == "/name") {
+                changeName(message.substring(6));
+            }
+            else if(message.split(" ")[0] == "/colour") {
+                changeColour(message.substring(8));
+            }
+            else {
+                socket.emit('new message', message);
+            }
+        }
+    }
+
+    const addMessage = (data) => {
+        let $timestampDiv = $('<span class="timestamp">').text(data.timestamp);
+        let $usernameDiv = $('<span class="username">').text(data.username).css('color', '#' + data.colour);
+        let $messageBodyDiv = $('<span class="messageBody">')
+            .text(data.message);
+        if (data.username == username) {
+            $messageBodyDiv = $('<span class="messageBodySelf">')
+                .text(data.message);
+        }
+        let $messageDiv = $('<li class="message">')
+            .append($timestampDiv, $usernameDiv, $messageBodyDiv);
+
+        addMessageElement($messageDiv);
+    }
+
+    const addMessageElement = (element) => {
+        let $element = $(element);
+        $messageArea.prepend($element);
+    }
+
+    $window.keydown(event => {
+        if (event.which === 13) {
+            sendMessage();
+        }
     });
-  });
+});
